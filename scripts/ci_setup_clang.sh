f6b8a7048ca9746ecc745ce22e41aec85708304a
M. Galib Uludag <m231@tnctr.com>
2024-03-31 22:06:34 +0300

    introduce bitmask support and std::format specialization

--- scripts/ci_setup_clang.sh
+++ scripts/ci_setup_clang.sh
@@ -7,6 +7,6 @@ VERSION=$1
 apt-get update
 apt-get install -y libc++-${VERSION}-dev libc++abi-${VERSION}-dev
 
-if [[ "${VERSION}" -ge 12 ]]; then
-    apt-get install -y --no-install-recommends libunwind-${VERSION}-dev
+if [[ ${VERSION} -ge 12 ]]; then
+	apt-get install -y --no-install-recommends libunwind-${VERSION}-dev
 fi

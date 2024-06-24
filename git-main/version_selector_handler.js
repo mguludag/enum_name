5eaf03fe7c9ddb9db98ccdcd3e5df00128a21643
mguludag <mguludag@users.noreply.github.com>
2024-06-24 19:12:14 +0000

    deploy: 93c12df4fb149c5306fe9d96e12bde43ffc2d604

--- git-main/version_selector_handler.js
+++ git-main/version_selector_handler.js
@@ -2,19 +2,22 @@
 // Copyright (c) 2024, Oleksandr Koval
 
 $(function () {
-    var repoName = window.location.pathname.split('/')[1];
-    $.get('/' + repoName + '/version_selector.html', function (data) {
-        // Inject version selector HTML into the page
-        $('#projectnumber').html(data);
+  var repoName = window.location.pathname.split("/")[1];
+  $.get("/" + repoName + "/version_selector.html", function (data) {
+    // Inject version selector HTML into the page
+    $("#projectnumber").html(data);
 
-        // Event listener to handle version selection
-        document.getElementById('versionSelector').addEventListener('change', function () {
-            var selectedVersion = this.value;
-            window.location.href = '/' + repoName + '/' + selectedVersion + '/index.html';
-        });
+    // Event listener to handle version selection
+    document
+      .getElementById("versionSelector")
+      .addEventListener("change", function () {
+        var selectedVersion = this.value;
+        window.location.href =
+          "/" + repoName + "/" + selectedVersion + "/index.html";
+      });
 
-        // Set the selected option based on the current version
-        var currentVersion = window.location.pathname.split('/')[2];
-        $('#versionSelector').val(currentVersion);
-    });
+    // Set the selected option based on the current version
+    var currentVersion = window.location.pathname.split("/")[2];
+    $("#versionSelector").val(currentVersion);
+  });
 });
